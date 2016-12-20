2d918fad6ea032df60dbd5790eb91183f46c134c
Jake Alsemgeest <jake.alsemgeest@scribblelive.com>
2016-12-19 19:30:50 -0500

    Added some Trees and Graphs.

--- TreesAndGraphs/src/graphs/Main.java
+++ TreesAndGraphs/src/graphs/Main.java
@@ -1,9 +1,110 @@
 package graphs;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+enum State {
+	Unvisited,
+	Visited,
+	Visiting;
+}
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		Graph g = createGraph(4);
+		System.out.println("\nCracking Code Interview 4.1:");
+		System.out.println(search(g, g.getNodes().get(0), g.getNodes().get(g.getNodes().size() - 1)));
 	}
 
+	
+	// Really just makes a list.
+	static Graph createGraph(int nodes) {
+		ArrayList<Node> list = new ArrayList<Node>();
+		
+		// Create all nodes.
+		for (int i = 0; i < nodes; i++) {
+			list.add(new Node(i));
+		}
+		
+		for (int i = 0; i < list.size() - 1; i++) {
+			list.get(i).addAdjacent(list.get(i+1));
+			list.get(i+1).addAdjacent(list.get(i));
+		}
+		
+		return new Graph(list);
+	}
+	
+	static boolean search(Graph g, Node start, Node end) {
+		 if (start == end) return true;
+		 
+		 LinkedList<Node> q = new LinkedList<Node>();
+		 
+		 for (Node u : g.getNodes()) {
+			 u.state = State.Unvisited;
+		 }
+		 start.state = State.Visited;
+		 q.add(start);
+		 Node u;
+		 while (!q.isEmpty()) {
+			 u = q.removeFirst();
+			 if (u != null) {
+				 for (Node v : u.getAdjacent()) {
+					 if (v.state == State.Unvisited) {
+						 if (v == end) {
+							 return true;
+						 } else {
+							 v.state = State.Visiting;
+							 q.push(v);
+						 }
+					 }
+				 }
+				 u.state = State.Visited;
+			 }
+		 }
+		 
+		 return false;
+	}
+	
+}
+
+class Graph {
+	private ArrayList<Node> nodes;
+	
+	public Graph() {
+		nodes = new ArrayList<Node>();
+	}
+	
+	public Graph(ArrayList<Node> l) {
+		nodes = l;
+	}
+	
+	public ArrayList<Node> getNodes() {
+		return nodes;
+	}
+}
+
+class Node {
+	public State state = State.Unvisited;
+	private ArrayList<Node> adjacent;
+	private int data;
+	
+	public Node() {
+		adjacent = new ArrayList<Node>();
+	}
+	
+	public Node(int i) {
+		data = i;
+		adjacent = new ArrayList<Node>();
+	}
+	
+	public void addAdjacent(Node n) {
+		if (!adjacent.contains(n)) {
+			adjacent.add(n);
+		}
+	}
+	
+	public ArrayList<Node> getAdjacent() {
+		return adjacent;
+	}
 }
--- TreesAndGraphs/src/trees/Main.java
+++ TreesAndGraphs/src/trees/Main.java
@@ -1,10 +1,89 @@
 package trees;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
 	// Chapter 4 (pg. 112)
 	// Answers on pg. 253
 	public static void main(String[] args) {
-
+		System.out.println("\nCracking Code Interview 4.2:");
+		System.out.println("\nCracking Code Interview 4.3:");
 	}
 
+	// Breadth First Search Implementation 4.3.
+	static ArrayList<LinkedList<TreeNode>> createLevelLinkedListBFS(TreeNode root) {
+		ArrayList<LinkedList<TreeNode>> result = new ArrayList<LinkedList<TreeNode>>();
+		LinkedList<TreeNode> list = new LinkedList<TreeNode>();
+		if (root != null) {
+			list.add(root);
+		}
+		
+		while (list.size() > 0) {
+			result.add(list);
+			LinkedList<TreeNode> parents = list;
+			list = new LinkedList<TreeNode>();
+			for (TreeNode parent : parents) {
+				if (parent.left != null) {
+					list.add(parent.left);
+				}
+				if (parent.right != null) {
+					list.add(parent.right);
+				}
+			}
+		}
+		return result;
+	}
+	
+	// Depth First Search Implementation 4.3.
+	static void createLevelLinkedListDFS(TreeNode root, ArrayList<LinkedList<TreeNode>> lists, int level) {
+		if (root == null) return; // Base case
+		
+		LinkedList<TreeNode> list = null;
+		if (lists.size() == level) {
+			// We do not have this level yet.
+			list = new LinkedList<TreeNode>();
+			lists.add(list);
+		} else {
+			list = lists.get(level);
+		}
+		
+		list.add(root);
+		createLevelLinkedListDFS(root.left, lists, level + 1);
+		createLevelLinkedListDFS(root.right, lists, level + 1);
+	}
+	
+	static ArrayList<LinkedList<TreeNode>> createLevelLinkedListDFS(TreeNode root) {
+		ArrayList<LinkedList<TreeNode>> list = new ArrayList<LinkedList<TreeNode>>();
+		createLevelLinkedListDFS(root, list, 0);
+		return list;
+	}
+	
+	static TreeNode createMinimalBST(int[] array) {
+		return  createMinimalBST(array, 0, array.length - 1);
+	}
+
+	private static TreeNode createMinimalBST(int[] array, int start, int end) {
+		if (start < end) return null;
+		int mid = (start + end) / 2;
+		TreeNode n = new TreeNode(array[mid]);
+		n.left = createMinimalBST(array, start, mid - 1);
+		n.right = createMinimalBST(array, mid + 1, end);
+		return n;
+	}
+	
+}
+
+class TreeNode {
+	public int data;
+	public TreeNode left = null;
+	public TreeNode right = null;
+	
+	public TreeNode() {
+		
+	}
+	
+	public TreeNode(int d) {
+		data = d;
+	}
 }
